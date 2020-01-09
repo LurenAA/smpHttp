568831e52e97229e97c8b5a87e34fee4a2d00dd5
lurenaa <978601499@qq.com>
2020-01-09 23:15:43 +0800

    update readme

--- src/HttpConnection.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "HttpConnection.hpp"
-#include <cstring>
-#include "FileLog.hpp"
-#include "Tools.hpp"
-
-using namespace std;
-using namespace xx;
--- src/smpHttp.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __SMP_HTTP__HPP_
-#define __SMP_HTTP__HPP_
-
-#include "HttpServer.hpp"
-#include "HttpResponse.hpp"
-#include "HttpRequest.hpp"
-#include "Util.hpp"
-
-#endif //__SMP_HTTP__HPP_
